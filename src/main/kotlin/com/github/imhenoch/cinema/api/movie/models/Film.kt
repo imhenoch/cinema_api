4729f8c2da75c37dc6e229a8cbd11eac21b163cd
Isidro Henoch <isidro.henoch@gmail.com>
2019-02-24 22:51:35 -0600

    Adds the film data model and the corresponding table structures

---
+++ src/main/kotlin/com/github/imhenoch/cinema/api/movie/models/Film.kt
@@ -0,0 +1,29 @@
+package com.github.imhenoch.cinema.api.movie.models
+
+import org.jetbrains.exposed.sql.Table
+
+object Films : Table() {
+    val id = integer("id").primaryKey().autoIncrement()
+    val name = varchar("name", 100)
+    val duration = long("duration")
+    val rating = reference("rating", Ratings.id)
+}
+
+object FilmLanguages : Table() {
+    val filmId = reference("film_id", Films.id).primaryKey(0)
+    val language = reference("language_id", Languages.id).primaryKey(1)
+}
+
+object FilmGenres : Table() {
+    val filmId = reference("film_id", Films.id).primaryKey(0)
+    val genreId = reference("genre_id", Genres.id).primaryKey(1)
+}
+
+data class Film(
+    val id: Int?,
+    val name: String,
+    val duration: Long,
+    val rating: Rating,
+    val languages: List<Language>,
+    val genres: List<Genre>
+)
